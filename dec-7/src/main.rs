13caf667515da48188fa0a0e0c30732f68b50539
TQ <tquinn86@github.com>
2022-12-09 12:28:37 -0500

    Dec 7 part 2

--- dec-7/src/main.rs
+++ dec-7/src/main.rs
@@ -24,6 +24,8 @@ fn main() {
     let mut i = 0;
     let max = 100000;
     let mut total : u64 = 0;
+
+    // Part 1
     loop {
         let item = match tree.get_at(i) {
             Some(i) => i,
@@ -43,5 +45,42 @@ fn main() {
 
 
     println!("Total of less that or equal to 100000: {}", total);
+
+    // Part 2 different calculation
+    // find the smallest directory that will get us to > 30000000 free space
+    // total space is 70000000. 7000000 - size of "/" is current free space
+
+    let total =   70000000;
+    let desired = 30000000;
+    let cur_free = total - tree.get_item_size(0).unwrap();
+    println!("Current free space: {}", cur_free);
+    let needed = desired - cur_free;
+
+    println!("Disk space needed: {}", needed);
+
+    let mut candidates: Vec<u64> = vec![];
+
+    i = 0;
+    loop {
+        let item = match tree.get_at(i) {
+            Some(i) => i,
+            None => break
+        };
+
+        if item.is_dir() {
+            let dir_size = tree.get_item_size(i).unwrap();
+            if dir_size >= needed {
+               println!("Directory {} size is {}", item.name(), dir_size);
+               candidates.push(dir_size);
+            } else {
+                println!("Directory {} of size {} is not enough to free up {} space", item.name(), dir_size, needed);
+            }
+        }
+        i += 1;
+    }
+
+    //sort and take the first (smallest) total
+    candidates.sort();
+    println!("Smallest directory that can free up enough space has a size of: {}", candidates[0]);
 }
 
